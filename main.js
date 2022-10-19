f307c8b6e7722e560036385c8992ed04f7237aa9
Ttst49 <thibautstachnick@gmail.com>
2022-10-19 11:36:16 +0200

    little modifs

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 const chiffres = document.querySelectorAll(".chiffre")
+const operands = document.querySelectorAll(".operateur")
 const ecran = document.querySelector("textarea")
 const boutonegal = document.querySelector('.egal')
-ecran.innerHTML= 0
-let result="0"
+const boutondel = document.querySelector('.del')
+ecran.innerHTML= ""
+let result= 0
 
 ecran.innerHTML=result
 
+function calculate() {
+    result=eval(ecran.innerHTML)
+    ecran.innerHTML=result
+}
+
 chiffres.forEach((el)=>{
   el.addEventListener('click',()=>{
+      if (ecran.innerHTML=="0"){
+          ecran.innerHTML =el.id
+      }else {
+          ecran.innerHTML += el.id
+      }
       console.log(el.id)
-      ecran.innerHTML += el.id
+    })
+})
+operands.forEach((el)=>{
+    el.addEventListener('click',()=>{
+        console.log(el.id)
+        ecran.innerHTML += el.id
     })
 })
 
-function calculate() {
-
-}
-
 boutonegal.addEventListener('click',()=>{
-    calculate();
+    console.log("on calcule")
+    calculate()
+
+})
+
+boutondel.addEventListener('click',()=>{
+    console.log("on supprime")
+    ecran.innerHTML="0"
+
 })
